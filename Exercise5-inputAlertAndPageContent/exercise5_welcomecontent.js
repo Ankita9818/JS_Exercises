8bbd49b0358d08892f798423852f00a28d518401
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-04 19:37:31 +0530

    Changed as per suggestions

--- Exercise5-inputAlertAndPageContent/exercise5_welcomecontent.js
+++ Exercise5-inputAlertAndPageContent/exercise5_welcomecontent.js
@@ -5,24 +5,30 @@ function PromptUserName(showContentElement) {
 }
 
 //function to prompt user
-PromptUserName.prototype.promptUserFunction = function() {
+PromptUserName.prototype.promptUser = function() {
   do {
     this.firstName = prompt('Please enter your First name ','');
-  } while(this.firstName == null || this.firstName.trim() == "");
+  } while(!this.firstName || (this.firstName = this.firstName.trim()) == "");
 
   do {
     this.lastName = prompt('Please enter your Last name ','');
-  } while(this.lastName == null || this.lastName.trim() == "");
+  } while(!this.lastName || (this.lastName = this.lastName.trim()) == "");
   this.showContent();
 };
 
 //Function to show alert and welcome content in page
 PromptUserName.prototype.showContent = function() {
-  var welcomeContent = 'Hello ' + this.firstName.trim() + ' ' + this.lastName.trim() + '.';
+  var welcomeContent = 'Hello ' + this.capitalizeFirstLetter(this.firstName) + ' ' + this.capitalizeFirstLetter(this.lastName);
   alert(welcomeContent);
   this.showContentElement.innerHTML = welcomeContent;
 };
 
+PromptUserName.prototype.capitalizeFirstLetter = function(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 //Object instantiated
-var PromptUserNameObject = new PromptUserName(document.querySelector("[data-label=content]"));
-PromptUserNameObject.promptUserFunction();
+window.onload = function() {
+  var promptUserNameObject = new PromptUserName(document.querySelector("[data-label=content]"));
+  promptUserNameObject.promptUser();
+}

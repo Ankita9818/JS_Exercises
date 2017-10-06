0587a9006c3163c1562bc5e8a9bfd947dfa49753
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-06 17:58:54 +0530

    Exercise9 Matching numerical values committed

---
+++ Exercise9-numericsRegex/javascript/exercise9_numerics_validation_regex.js
@@ -0,0 +1,40 @@
+//Object Constructor
+function NumericsValidator(formElementsNamesHash) {
+  this.formName = formElementsNamesHash.form;
+  this.formElement = document.forms[this.formName];
+  this.numberFieldElement = this.formElement[formElementsNamesHash.numberField];
+  this.resultFieldElement = this.formElement[formElementsNamesHash.resultField];
+}
+
+NumericsValidator.prototype.init = function(){
+  var _this = this;
+  this.formElement.addEventListener('submit', function(submitEvent) {
+    var valid = _this.validateNumericalCharacters();
+    if(!valid) {
+      submitEvent.preventDefault();
+    }
+  });
+};
+
+//Function which validates that input has only numeric characters
+NumericsValidator.prototype.validateNumericalCharacters = function() {
+  var validNumericRegEx = /^\d+\.?\d+$/;
+  var valid = false;
+  if(this.numberFieldElement.value.match(validNumericRegEx)) {
+    valid = true;
+  }
+  this.resultFieldElement.value =  valid;
+  return valid;
+}
+
+var formElementsNames = {
+  form: 'form1',
+  numberField: 'numberInput',
+  resultField: 'result'
+};
+
+//Object intantiated
+window.onload = function() {
+  var numericsValidatorObject = new NumericsValidator(formElementsNames);
+  numericsValidatorObject.init();
+};

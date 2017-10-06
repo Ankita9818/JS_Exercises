d1c4bfc4b739228b0fbce2f35b53e022e6336131
Ankita9818 <ankita.dixit@vinsol.com>
2017-10-06 10:30:26 +0530

    Changed as per suggestions

--- Exercise6-open_valid_url_window/exercise6_open_valid_url.js
+++ Exercise6-open_valid_url_window/exercise6_open_valid_url.js
@@ -1,19 +1,17 @@
-function UrlPromptor() {
-  this.url = "";
-}
+function UrlPromptor() {}
 
 //function to prompt user for url
-UrlPromptor.prototype.promptUserFunction = function() {
-  this.url = prompt('Please enter the url you want to open','');
+UrlPromptor.prototype.init = function() {
+  this.url = prompt('Please enter the url you want to open');
   if(this.url == null || this.url.trim() == "") {
     alert('The url is invalid');
   } else {
-    window.open(this.url,'newWindow','height=450,width=400,scrollbars=no,status=no,toolbar=no,menubar=no,titlebar=no,location=no');
+    window.open(this.url,'_blank','height=450,width=400,scrollbars=no,status=no,toolbar=no,menubar=no,titlebar=no,location=no');
   }
 };
 
 //Object instantiated
 window.onload = function() {
   var urlPromptorObject = new UrlPromptor();
-  urlPromptorObject.promptUserFunction();
-}
+  urlPromptorObject.init();
+};
